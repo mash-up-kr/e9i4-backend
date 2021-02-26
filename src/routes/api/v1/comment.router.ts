57dfbee7d402a4597cae3c43f8bb1d1371406cb1
swimjiy <swimjiy@gmail.com>
2021-02-26 23:33:37 +0900

    Remove unused files

--- src/routes/api/v1/comment.router.ts
+++
@@ -1,9 +0,0 @@
-import express, {Router} from 'express';
-import * as commentController from '../../../controllers/comment.controller';
-
-const router: Router = express.Router();
-
-router.delete('/:postId/comments/:commentId', commentController.deleteComment);
-router.post('/:postId/comments', commentController.addComment);
-
-export default router;
