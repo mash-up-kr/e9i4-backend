c0e160ab603185c2df6e9ead725cdb904ff51de3
Swimjiy <swimjiy@gmail.com>
2021-02-28 00:37:51 +0900

    Add Category API (#1)

--- src/routes/api/v1/index.ts
+++ src/routes/api/v1/index.ts
@@ -1,9 +1,11 @@
 // /api/v1 라우팅
 import express, {Router} from 'express';
 import userRouter from './user.router';
+import categoryRouter from './category.router';
 
 const router: Router = express.Router();
 
 router.use('/user', userRouter);
+router.use('/category', categoryRouter);
 
 export default router;
