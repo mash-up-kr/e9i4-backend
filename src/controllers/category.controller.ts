7480058ade7cec41043c4519150aad4435cb5fd4
jiyoung.lim <swimjiy@krafton.com>
2021-03-22 17:51:40 +0900

    Change response format

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -4,7 +4,7 @@ import * as categoryService from '../services/category.service';
 export async function getCategories(req: Request, res: Response) {
   try {
     const categories = await categoryService.getCategories();
-    res.status(200).send(categories);
+    res.status(200).send({data: categories});
   } catch (err) {
     res.status(500).send('Error while find categories');
   }
