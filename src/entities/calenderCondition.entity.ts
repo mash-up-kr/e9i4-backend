a09d93010bd0146fae13b769dbd2b8b0cde9a44b
god9599 <59276134+god9599@users.noreply.github.com>
2021-03-27 01:55:00 +0900

    Add calendarCondition entity

---
+++ src/entities/calenderCondition.entity.ts
@@ -0,0 +1,45 @@
+import {
+  Entity,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  JoinColumn,
+  OneToMany,
+  ManyToOne,
+  JoinTable,
+} from 'typeorm';
+import {Alarm} from './alarm.entity';
+
+@Entity()
+export class CalenderCondition extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: bigint;
+
+  @Column({nullable: true})
+  year: number;
+
+  @Column({nullable: true})
+  month: number;
+
+  @Column({nullable: true})
+  dayOfMonth: number;
+
+  @Column({nullable: true})
+  hour: number;
+
+  @Column({nullable: true})
+  minute: number;
+
+  @Column({nullable: true})
+  second: number;
+
+  @Column({nullable: true, select: false})
+  alarmId: bigint;
+
+  @ManyToOne(() => Alarm, alarm => alarm.calenderConditions)
+  alarm: Alarm[];
+
+  @JoinTable()
+  alarms: Alarm[];
+}
