3dd0d5b276ce36a3b7449be3172c045f8d4d700e
god9599 <59276134+god9599@users.noreply.github.com>
2021-04-10 16:27:54 +0900

    Fix lint error(Unnecessary module)

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,4 @@
-import {PlatformType, User} from '../entities/user.entity';
+import {User} from '../entities/user.entity';
 import * as jwt from 'jsonwebtoken';
 
 export function createJwtToken(user: User) {
